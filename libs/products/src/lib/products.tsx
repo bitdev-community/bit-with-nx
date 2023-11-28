fbc75edccc15050c6b3aa2ae2946fc41bfc0af06
ashanfernando <ashan256@gmail.com>
2023-11-28 18:13:04 +0530

    Adding component to NX

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,4 +1,5 @@
 import styles from './products.module.css';
+import { Button } from '@ashanfernando/bit-with-nx.ui.button';
 
 /* eslint-disable-next-line */
 export interface ProductsProps {}
@@ -7,6 +8,7 @@ export function Products(props: ProductsProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Products!</h1>
+      <Button>Add Products</Button>
     </div>
   );
 }
