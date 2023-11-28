d7e7118057306dc2818ed3695de6a5ab648c9b6f
ashanfernando <ashan256@gmail.com>
2023-11-28 17:52:16 +0530

    Exporting components

---
+++ bit-components/bit-with-nx/ui/button/button.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React, { ReactNode } from 'react';
+
+export type ButtonProps = {
+  /**
+   * a node to be rendered in the special component.
+   */
+  children?: ReactNode;
+};
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
